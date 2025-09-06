dc48a1845a3e7db2b8d6df4a8a925704b65e684b
Leonardo Lima <leonardolimastos2003@gmail.com>
2025-09-06 11:24:43 -0300

    Configurando cliente e resposta do assistente GPT

--- main.py
+++ main.py
@@ -4,3 +4,22 @@ import os
 
 load_dotenv()
 MY_KEY_OPENAI = os.getenv("MY_KEY_OPENAI")
+
+cliente = OpenAI(api_key=os.getenv("MY_KEY_OPENAI"))
+
+response = cliente.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "Listar apenas os nomes dos produtos, sem considerar a descrição."
+        },
+        {
+            "role": "user",
+            "content" : "Liste 3 produtos sustentáveis."
+    
+        },
+    ],
+    model="gpt-4"
+)
+
+print(response.choices[0].message.content)
