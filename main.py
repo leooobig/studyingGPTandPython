6a61d9f5cab09d68c63305459dee3f9c3c0ef6ae
Leonardo Lima <leonardolimastos2003@gmail.com>
2025-09-11 17:19:50 -0300

    Dinclear ; python -u

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from categorizador import categorizador
 
-produto = str(input('Digite o produto para ser classificado:'))
-categorizador(produto=produto)
+if __name__ == "__main__":
+    categorizador()
+
